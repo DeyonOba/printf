546cae2fc2b58534e2db0e3fc6667b482d56c792
Okafor Patrick <patrickpeters911@gmail.com>
2023-08-20 17:25:39 -0700

    Write a function that produces output according to a format.

---
+++ main.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+#ifndef _MAIN
+#define _MAIN
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+void printNumber(int num);
+
+
+#endif

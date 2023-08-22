de08d2c8fe903b60a874274bfabaf9fe98eb61fe
Gideon Oba <deyon@gmail.com>
2023-08-22 14:04:32 +0000

    refactor: main.h to betty code styling

--- main.h
+++ main.h
@@ -1,26 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <limits.h>
 
-#ifndef _MAIN
-#define _MAIN
-
 int _putchar(char c);
 int _printf(const char *format, ...);
 int int_to_string(int num, char *buffer, int bufferSize);
-
-typedef struct
+/**
+ * struct specify - struct defines a specifier and it function pointer
+ * @specifier: character specifier
+ * @func: function pointer
+ */
+typedef struct specify
 {
 	char specifier;
 	void (*func)(va_list ap, int *count);
-}spec;
+} spec;
 
 void print_char(va_list ap, int *count);
 void print_string(va_list ap, int *count);
 void print_percent(va_list ap, int *count);
 void print_integer(va_list ap, int *count);
-
 
 #endif
